2946da659cc391a7f93ba84e02e755bdf2efd891
Stoyan <stoyaneft@gmail.com>
2014-12-03 18:41:52 +0200

    Search engine

--- page.py
+++ page.py
@@ -1,13 +1,16 @@
 from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship
+from sqlalchemy import ForeignKey
 from connections import *
 
 
 class Page(Base):
     __tablename__ = "pages"
     id = Column(Integer, primary_key=True)
-    website = Column(String)
     title = Column(String)
     description = Column(Integer)
     url = Column(String)
     ssl = Column(Boolean)
     points = Column(Integer)
+    website_url = Column(String, ForeignKey("websites.url"))
+    website = relationship("Website", backref="pages")
--- spider.py
+++ spider.py
@@ -1,7 +1,8 @@
 import requests
 import urllib
 from urllib.parse import urlparse
-
+import re
+import bs4
 from bs4 import BeautifulSoup
 
 from page import Page
@@ -20,6 +21,7 @@ class Spider():
         self.website_info = self.get_website_info()
         self.pages_info = {}
         self.scan_depth = scan_depth
+        self.to_scan = [website]
 
     @staticmethod
     def get_domain(url):
@@ -33,15 +35,16 @@ class Spider():
         soup = BeautifulSoup(html)
         info = {'url': self.website}
         info['title'] = soup.title.string if soup.title else None
-        info['description'] = soup.description.string if soup.description else None
         info['pages_count'] = 0
+        info['HTML_version'] = self.get_HTML_version()
         return info
-        print(info)
 
     def get_page_info(self, soup):
         info = {}
         info['title'] = soup.title.string if soup.title else None
-        info['description'] = soup.description.string if soup.description else None
+        description = soup.find(attrs={"property": "og:description"})
+        if description:
+            info['description'] = description['content'].encode('utf-8')
         return info
 
     def is_outgoing(self, url):
@@ -52,44 +55,64 @@ class Spider():
     def prepare_url(self, url, href):
         return urllib.parse.urljoin(url, href)
 
+    def get_HTML_version(self):
+        soup = BeautifulSoup(requests.get(self.website).text)
+        items = [
+            item for item in soup.contents if isinstance(item, bs4.Doctype)]
+        doctype = items[0] if items else None
+        html_version = re.search(
+            'HTML (?P<version>\d+.\d+)', doctype).group('version')
+        return html_version
+
     def save_page_info(self, url, soup):
         page_info = self.get_page_info(soup)
         pages_count = self.website_info['pages_count']
         self.pages_info[pages_count] = page_info
         self.pages_info[pages_count]['url'] = url
+        self.pages_info[pages_count]['website_url'] = self.website
 
-    def scan_website(self, url):
-        if url not in self.scanned_urls:
-            print(url)
-            self.scanned_urls.append(url)
-            r = requests.get(url)
-            html = r.text
-            soup = BeautifulSoup(html)
-            self.website_info['pages_count'] += 1
-            self.save_page_info(url, soup)
-            for link in soup.find_all('a'):
-                new_link = self.prepare_url(url, link.get('href'))
-                if not self.is_outgoing(new_link):
-                    self.scan_website(new_link)
-                if self.scan_depth:
-                    if self.website_info['pages_count'] == self.scan_depth:
-                        return
+    def should_be_scanned(self, url):
+        return url not in self.scanned_urls \
+            and not self.is_outgoing(url) and '#' not in url
+
+    def scan_page(self, url):
+        print(url)
+        self.scanned_urls.append(url)
+        r = requests.get(url)
+        html = r.text
+        soup = BeautifulSoup(html)
+        self.website_info['pages_count'] += 1
+        self.save_page_info(url, soup)
+        for link in soup.find_all('a'):
+            new_link = self.prepare_url(url, link.get('href'))
+            if self.should_be_scanned(new_link):
+                self.to_scan.append(new_link)
+
+    def scan_website(self):
+        while len(self.to_scan) != 0:
+            self.scan_page(self.to_scan.pop())
+            if self.scan_depth:
+                if self.website_info['pages_count'] == self.scan_depth:
+                    return
 
     def save_in_db(self):
-        urls = [url[0] for url in self.__session.query(Page.url).all()]
+        saved_urls = [url[0] for url in self.__session.query(Page.url).all()]
         for page in self.pages_info.values():
-            if page['url'] in urls:
-                return
+            if page['url'] in saved_urls:
+                continue
             pg = Page(**page)
             self.__session.add(pg)
+        saved_websites = [site[0]
+                          for site in self.__session.query(Website.url).all()]
+        if self.website not in saved_websites:
+            self.__session.add(Website(**self.website_info))
         self.__session.commit()
 
 
 def main():
-    spider = Spider('http://hackbulgaria.com/', 5)
-    spider.scan_website('http://hackbulgaria.com/')
+    spider = Spider('https://www.mattcutts.com/blog/hermit-mode/', 10)
+    spider.scan_website()
     spider.save_in_db()
-    print(spider.pages_info)
 
 if __name__ == '__main__':
     main()
